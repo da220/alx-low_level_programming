aece931758f6c0cba4f0bee15af7be1ce23e18d2
da220 <danielosongo279@gmail.com>
2023-06-19 06:26:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  program that prints exactly with line printf function
+ * return: 0 (success)
+ * compiles without warning when using -wall gcc
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
