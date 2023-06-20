0723d5f7d2cce4b17886fbf1b1d77d965f29e530
da220 <danielosongo279@gmail.com>
2023-06-20 05:21:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
- * Return: Always 0 (success)
+ * main - Aprogramm hst assigns a random number to a variable after execution.
+ *
+ * Return: 0 (success)
  */
 int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	(
+	{
 		printf("%d is positive\n", n);
-	)
+	}
 	else if (n == 0)
-	(
-	 printf("%d is zero\n", n);
-	)
+	{
+		printf("%d is zero\n", n);
+	}
 	else
-	(
-	 printf("%d is negative\n", n);
-	)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
-
