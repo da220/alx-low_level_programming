871d74404b8c68a823ef0571ff8580b36b5f1fb1
da220 <danielosongo279@gmail.com>
2023-07-18 06:35:59 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,23 +10,14 @@
  * Return: Always 0 (success)
 */
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-
-
 	for (i = 0; i < argc; i++)
-
 	{
-
 		printf("%s\n", argv[i]);
-
 	}
-
-
-
 	return (0);
-
 }
 
