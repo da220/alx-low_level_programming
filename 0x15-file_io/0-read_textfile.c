f4b489b3de7dca248a3404876722889f03b64b74
DanielSE <126680319+da220@users.noreply.github.com>
2024-02-13 11:49:57 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * read_textfile - prints to the stdout POSIX after reading a textfile
+ * @filename: pointer to the file name
+ * @letters: number of letters to be read and print
  *
- * Return: Always 0.
+ * Return: no of letters it could print or 0
  */
-int main(int ac, char **av)
-{
-    ssize_t n;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_Des, turn_rd, turn_wr;
+	char *sto_size;
+
+	if (filename == NULL)
+		return (0);
+
+	file_Des = open(filename, O_RDONLY);
+	if (file_Des == -1)
+		return (0);
+	sto_size = malloc(sizeof(char) * letters + 1);
+	if (sto_size == NULL)
+		return (0);
+	turn_rd = read(file_Des, sto_size, letters);
+	if (turn_rd == -1)
+		return (0);
+
+	sto_size[letters] = '\0';
+
+	turn_wr = write(STDOUT_FILENO, sto_size, turn_rd);
+	if (turn_wr == -1)
+		return (0);
+
+	close(file_Des);
+	free(sto_size);
+	return (turn_wr);
 }
